66d87d243ffaad2e85f627a6ad8b45d51a5449ba
samescolas <samescolas@gmail.com>
2017-09-05 15:56:37 -0700

    Meaningless change.

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-	res.send('hello, world!');
+	res.send('<h1>hello, world!</h1>');
 });
 
 app.listen(PORT, HOST);
