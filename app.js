caa8168e107bda29620fc21cf471dc90fb561688
Sam Escolas <samescolas@gmail.com>
2017-09-06 01:58:18 +0000

    How do I reeech these kids?

--- app.js
+++ app.js
@@ -8,8 +8,23 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
+
+// Define header
+app.all('/*', function (req, res, next) {
+	res.msg = '<h1>Matcha</h1>';
+	next();
+});
+
 app.get('/', (req, res) => {
-	res.send('<h1>hello, world!</h1>');
+	res.send(res.msg + '<h1>hello, world!</h1>');
+});
+
+app.get('/about', (req, res) => {
+	res.send(res.msg + '<h1>About</h1>');
+});
+
+app.get('/testing', (req, res) => {
+	res.send(res.msg + 'Testing page');
 });
 
 app.listen(PORT, HOST);
