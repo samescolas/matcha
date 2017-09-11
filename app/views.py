06a81995a8839b6ad1bab0a11f5b70b8fac3160b
Sam Escolas <samescolas@gmail.com>
2017-09-10 17:59:06 -0700

    Adding views.py

--- app/views.py
+++ app/views.py
@@ -1,10 +1,15 @@
+from flask import render_template, flash, redirect
 from app import app
+from .forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
 	return 'Hello, world!'
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-	return '<h1>Login</h1>'
+	form = LoginForm()
+	return render_template( 'login.html', 
+				title='Sign In', 
+				form=form)
