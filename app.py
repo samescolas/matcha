18b719012de01b7504c9014341c334aea3c8652e
root <root@7d9b0673fc12.(none)>
2017-09-16 21:45:49 +0000

    Adding a variable route.

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello():
 	return 'hello, world!'
 
+@app.route('/<name>')
+def hello_name(name):
+	return 'hello, {}!'.format(name)
+
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=80)
