e3f4d246e6e2d64285474fdbd82d8c8a3a233a53
Sam Escolas <samescolas@gmail.com>
2017-09-17 17:37:46 +0000

    Testing db.get inside user class.

--- run.py
+++ run.py
@@ -1,19 +1,47 @@
 from flask import Flask
 from app import Database
+from app import User
 
 app = Flask(__name__)
 db = Database('localhost', 'matcha', 'matcha', 'matcha')
 
-db.put('interests', { 'label': 'Soccer'})
-print(db.get('interests', 'id', 14))
+# Example of db.put
+stored = db.put('interests', { 'label': 'Coding'})
+if stored == None:
+	print("Unable to insert data.")
+else:
+	print("Stored ID: {}".format(stored))
+
+# Example of db.get
+result = db.get('interests', 'id', '1')
+if result:
+	for record in db.results:
+		for field in record:
+			print(field)
+
+# Example of db.query
+another = db.query("SELECT * FROM interests WHERE ID = (%s)", [11])
+if another:
+	print(db.results)
+else:
+	print 'No dice.'
 
 @app.route('/')
 def hello():
 	return 'hello, world!'
 
-@app.route('/<name>')
+@app.route('/<name>/')
 def hello_name(name):
-	return 'hello, {}!'.format(name)
+	print 'name: {}'.format(name)
+	user = User(db, name, 'test')
+	if user.available:
+		return 'Welcome, {}!'.format(name)
+	else:
+		return 'Username {} already exists.'.format(name)
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=80)
+
+@app.route('/favicon.ico')
+def favicon():
+	return None
