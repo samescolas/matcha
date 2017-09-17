4a27229af508b02ef1a41955ff70eb69220d9162
samescolas <samescolas@gmail.com>
2017-09-16 21:11:13 -0700

    Minor changes.

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM debian:wheezy
+FROM debian:latest
 
 ENV APP_DIR /var/www/matcha
 ENV FLASK_APP $APP_DIR/run.py
@@ -6,12 +6,11 @@ ENV FLASK_APP $APP_DIR/run.py
 RUN mkdir -p $APP_DIR
 COPY ./ $APP_DIR
 
-RUN cp $APP_DIR/.[vb]* /root/
+RUN mv $APP_DIR/.[vb]* /root/
 
 RUN apt-get update && apt-get upgrade && apt-get install -y \
 	apache2 \
 	libapache2-mod-wsgi \
-	mysql-client \
 	mysql-server \
 	python3 \
 	python-pip \
@@ -19,5 +18,10 @@ RUN apt-get update && apt-get upgrade && apt-get install -y \
 	vim
 
 RUN pip install Flask && pip install Flask-PyMongo && pip install flask_wtf
+
+RUN service mysql start && \
+		mysqladmin -u root password matcha && \
+		mysqladmin create matcha && \
+		mysql -u root -e "$(cat $APP_DIR/init.sql)"
 
 ENTRYPOINT bash
---
+++ init.sql
@@ -0,0 +1,74 @@
+CREATE USER 'matcha'@'localhost' IDENTIFIED BY 'matcha';
+
+GRANT ALL PRIVILEGES ON *.* TO 'matcha'@'localhost';
+
+FLUSH PRIVILEGES;
+
+USE matcha;
+
+CREATE TABLE users (
+	id INT NOT NULL AUTO_INCREMENT,
+	username VARCHAR(18) NOT NULL, 
+	email VARCHAR(32) NOT NULL,
+	dt_joined DATETIME DEFAULT NOW(),
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE shadow (
+	id INT NOT NULL AUTO_INCREMENT,
+	user_id INT NOT NULL,
+	passwd CHAR(64),
+	salt CHAR(32),
+	PRIMARY KEY (id),
+	FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE sessions (
+	id INT NOT NULL AUTO_INCREMENT,
+	user_id INT NOT NULL,
+	hash CHAR(64) NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE images (
+	id INT NOT NULL AUTO_INCREMENT,
+	user_id INT NOT NULL,
+	location VARCHAR(96) NOT NULL,
+	image_type VARCHAR(12) NOT NULL,
+	title VARCHAR(32),
+	description TEXT,
+	creation_dt DATETIME  NOT NULL DEFAULT NOW(),
+	PRIMARY KEY (id),
+	FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE likes (
+	id INT NOT NULL AUTO_INCREMENT,
+	user_id INT NOT NULL,
+	image_id INT NOT NULL,
+	dt DATETIME NOT NULL DEFAULT NOW(),
+	PRIMARY KEY (id),
+	UNIQUE(user_id, image_id),
+	FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+	FOREIGN KEY (image_id) REFERENCES images(id) ON DELETE CASCADE
+);
+
+CREATE TABLE email_verification (
+	id INT NOT NULL AUTO_INCREMENT,
+	user_id INT NOT NULL,
+	token VARCHAR(64),
+	PRIMARY KEY (id),
+	FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE comments (
+	id INT NOT NULL AUTO_INCREMENT,
+	user_id INT NOT NULL,
+	image_id INT NOT NULL,
+	comment TEXT NOT NULL,
+	dt DATETIME NOT NULL DEFAULT NOW(),
+	PRIMARY KEY (id),
+	FOREIGN KEY (user_id) REFERENCES users(id),
+	FOREIGN KEY (image_id) REFERENCES images(id) ON DELETE CASCADE
+);
