e4ec3fff787bf965dff24108c8abc9e69d5a6951
Sam Escolas <samescolas@gmail.com>
2017-09-17 18:09:04 +0000

    Adding create method to user class.

--- app/models/user.py
+++ app/models/user.py
@@ -2,5 +2,14 @@ class User:
 
 	def __init__(self, db, email, passwd):
 		self.db = db
-		print 'inside user with email {} and pass {}'.format(email, passwd)
+		self.email = email
+		self.passwd = passwd
 		self.available = not self.db.get('users', 'email', email)
+		self.user_id = 0
+
+	def create(self):
+		self.user_id = self.db.put('users', { 'email': self.email })
+		if self.user_id == None:
+			return False
+		return True
+			
