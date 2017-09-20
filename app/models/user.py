8dd60ca82cb4b16c26506ce126b602000ae8ae3f
Sam Escolas <samescolas@gmail.com>
2017-09-20 21:49:10 +0000

    Closing database connection in __del__ method.

--- app/models/user.py
+++ app/models/user.py
@@ -1,13 +1,18 @@
 from . import Database
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User:
 
 	def __init__(self, email, passwd):
 		self.db = Database('localhost', 'matcha', 'matcha', 'matcha')
 		self.email = email
-		self.passwd = passwd
+		self.passwd = self.hash_password(passwd)
 		self.available = not self.db.get('users', 'email', email)
+		self.data = self.db.results
 		self.user_id = 0
+
+	def __del__(self):
+		self.db.close_connection()
 
 	def add(self):
 		self.user_id = self.db.put('users', { 'email': self.email })
@@ -16,4 +21,7 @@ class User:
 		return True
 	def auth(self):
 		self.is_logged_in = True
-			
+
+	@staticmethod
+	def hash_password(password):
+		return generate_password_hash(password, 'pbkdf2:sha256', salt_length=17)
