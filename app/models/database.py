882e8917d97ecabe9165e41bc6331204f24b139b
Sam Escolas <samescolas@gmail.com>
2017-09-21 23:21:15 +0000

    Removing print statments

--- app/models/database.py
+++ app/models/database.py
@@ -41,7 +41,6 @@ class Database:
 		sql = sql[:-2] + ');'
 		for key in fields:
 			params.append(fields[key])
-		print("SQL: {}".format(sql))
 		print(params)
 		try:
 			self.curr.execute(sql, params)
