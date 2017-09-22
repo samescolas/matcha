5b27029fca2ef9a94dee816036b32ffdddd6a9db
Sam Escolas <samescolas@gmail.com>
2017-09-22 22:41:31 +0000

    Adding home route to server. Will be moving all routing logic to frontend.

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-from flask import Flask, request, url_for, session, redirect, make_response
+from flask import Flask, request, url_for, session, redirect, make_response, render_template
 #from flask_wtf.csrf import CSRFProtect
 from app import User, auth, api
 
@@ -8,6 +8,9 @@ app = Flask(__name__)
 # CSRF protection
 app.secret_key = 's3cret'
 #csrf = CSRFProtect(app)
+
+# Set templates folder
+app.template_folder = "./static"
 
 # Auth contains all routing from / until logged in.
 app.register_blueprint(auth)
@@ -18,6 +21,10 @@ app.register_blueprint(api)
 #@app.route('/favicon.ico')
 #def favicon():
 #	return send_from_directory()
+
+@app.route('/')
+def home():
+	return render_template('index.html')
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=80)
