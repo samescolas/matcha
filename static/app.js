3c9ca8fdb9b678b0c887b218a5c963565fde2099
Sam Escolas <samescolas@gmail.com>
2017-09-23 02:41:33 +0000

    Testing Angular routing with ngRoute.

--- static/app.js
+++ static/app.js
@@ -1,47 +1,20 @@
 // app.js
-// create our angular app and inject ngAnimate and ui-router 
+// create our angular app
 // =============================================================================
-angular.module('formApp', ['ngAnimate', 'ui.router'])
+var app = angular.module('formApp', ["ngRoute"]);
 
-// configuring our routes 
-// =============================================================================
-.config(function($stateProvider, $urlRouterProvider) {
-
-	$stateProvider
-
-        // route to show our basic form (/form)
-	.state('form', {
-		url: '/form',
-		templateUrl: 'static/form.html',
-		controller: 'formController'
+app.config(function($routeProvider) {
+	$routeProvider
+	.when("/", {
+		templateUrl : "static/form-profile.html"
 	})
-
-	// nested states 
-	// each of these sections will have their own view
-	// url will be nested (/form/profile)
-	.state('form.profile', {
-		url: '/profile',
-		templateUrl: 'static/form-profile.html'
-	})
-
-	// url will be /form/interests
-	.state('form.interests', {
-		url: '/interests',
-		templateUrl: 'static/form-interests.html'
-	})
-
-	// url will be /form/payment
-	.state('form.payment', {
-		url: '/payment',
-		templateUrl: 'static/form-payment.html'
+	.when("/payment", {
+		templateUrl: "static/form-payment.html"
 	});
+});
 
-	$urlRouterProvider.otherwise('/form/profile');
-})
-
-// our controller for the form
 // =============================================================================
-.controller('formController', function($scope) {
+app.controller('formController', function($scope) {
 
 	// we will store all of our form data in this object
 	$scope.formData = {};
